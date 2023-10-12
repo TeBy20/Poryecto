98eb702dce549bc68ccae9d8514219da131aff58
Ezequiel Rueda <123047967+1millonusd@users.noreply.github.com>
2023-10-12 01:06:55 -0300

    Update ZonasController.php

--- app/Http/Controllers/ZonasController.php
+++ app/Http/Controllers/ZonasController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Zonas;
 use Illuminate\Http\Request;
 
-
 class ZonasController extends Controller
 {
     public function indexZonas()
@@ -22,9 +21,28 @@ class ZonasController extends Controller
 
     public function store(Request $request)
     {
+        // Definir reglas de validación
+        $rules = [
+            'nombre' => 'required|max:255', // Establecer el límite en 255 caracteres, puedes ajustarlo según tus necesidades
+            'descripcion' => 'required',
+            // Agrega más reglas de validación según los campos de tu modelo Zonas
+        ];
+
+        // Mensajes personalizados para las reglas de validación
+        $messages = [
+            'nombre.required' => 'El nombre de la zona es obligatorio.',
+            'nombre.max' => 'El nombre de la zona no puede tener más de 255 caracteres.',
+            'descripcion.required' => 'La descripción de la zona es obligatoria.',
+            // Agrega más mensajes según las reglas de validación que agregues
+        ];
+
+        // Validar la solicitud
+        $request->validate($rules, $messages);
+
+        // Crear la zona si pasa la validación
         Zonas::create($request->all());
 
-        return redirect()->route('zonas.indexZonas')->with('status', 'Zona creada stisfactoriamente');
+        return redirect()->route('zonas.indexZonas')->with('status', 'Zona creada satisfactoriamente');
     }
 
     public function edit($id)
@@ -35,11 +53,28 @@ class ZonasController extends Controller
 
     public function update(Request $request, $id)
     {
-        $zonas = Zonas::findOrFail($id);
+        // Definir reglas de validación para la actualización
+        $rules = [
+            'nombre' => 'required|max:255',
+            'descripcion' => 'required',
+            // Agrega más reglas de validación según los campos de tu modelo Zonas
+        ];
 
+        // Mensajes personalizados para las reglas de validación
+        $messages = [
+            'nombre.required' => 'El nombre de la zona es obligatorio.',
+            'nombre.max' => 'El nombre de la zona no puede tener más de 255 caracteres.',
+            'descripcion.required' => 'La descripción de la zona es obligatoria.',
+            // Agrega más mensajes según las reglas de validación que agregues
+        ];
+
+        // Validar la solicitud de actualización
+        $request->validate($rules, $messages);
+
+        $zonas = Zonas::findOrFail($id);
         $zonas->update($request->all());
 
-        return redirect()->route("zonas.indexZonas")->with("status", "Zona actualizado satisfactoriamente!");
+        return redirect()->route("zonas.indexZonas")->with("status", "Zona actualizada satisfactoriamente!");
     }
 
     public function destroy($id)
@@ -51,3 +86,4 @@ class ZonasController extends Controller
         return redirect()->route('zonas.indexZonas')->with('status', 'Zona eliminada satisfactoriamente');
     }
 }
+
